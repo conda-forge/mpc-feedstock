deeaba68b09ad3b1b4f5c6acf1aa83808316ed43
Isuru Fernando <isuruf@gmail.com>
2021-09-13 01:13:53 -0500

    use autotools_clang_conda on win

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 # Get an updated config.sub and config.guess
-# Delete first because they have weird permissions
-rm -f config.sub
-rm -f config.guess
-cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
+cp $BUILD_PREFIX/share/gnuconfig/config.* build-aux/ || true
+
+if [[ "$target_platform" == "win-64" ]]; then
+  export CPPFLAGS="$CPPFLAGS -DMSC_USE_DLL"
+  sed -i.bak "s@-Wl,--output-def,.libs/libmpfr-6.dll.def@@g" configure
+  sed -i.bak "s@ -version-info [0-9\:]\+@ -avoid-version@g" src/Makefile.in
+fi
 
 ./configure --prefix=$PREFIX \
             --with-gmp=$PREFIX \
             --with-mpfr=$PREFIX \
             --disable-static
+
+[[ "$target_platform" == "win-64" ]] && patch_libtool
 
 make -j${CPU_COUNT}
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" != 1 ]]; then
