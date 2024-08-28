fd7dd265284a4b7b37b89fa6d5fa73edfefdc8a6
Isuru Fernando <isuruf@gmail.com>
2024-08-28 09:15:40 -0500

    Use gmp

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,7 @@
 cp $BUILD_PREFIX/share/gnuconfig/config.* build-aux/ || true
 
 if [[ "$target_platform" == "win-64" ]]; then
-  export CPPFLAGS="$CPPFLAGS -DMSC_USE_DLL"
-  sed -i.bak "s@-Wl,--output-def,.libs/libmpfr-6.dll.def@@g" configure
-  sed -i.bak "s@ -version-info [0-9\:]\+@ -avoid-version@g" src/Makefile.in
+  export PREFIX=${PREFIX}/Library
 fi
 
 ./configure --prefix=$PREFIX \
@@ -14,10 +12,12 @@ fi
             --with-mpfr=$PREFIX \
             --disable-static
 
-[[ "$target_platform" == "win-64" ]] && patch_libtool
-
 make -j${CPU_COUNT}
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" != 1 ]]; then
   make check -j${CPU_COUNT}
 fi
 make install
+
+if [[ "$target_platform" == "win-64" ]]; then
+  cp ${PREFIX}/lib/libmpfr.dll.a ${PREFIX}/lib/mpfr.lib
+fi
